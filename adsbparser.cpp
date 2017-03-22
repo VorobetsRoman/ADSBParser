7861a82429e3ff7047a9b0aec56bcd5d429e11b4
VorobetsRoman <vorobetsroman@mail.ru>
2017-03-22 17:57:14 +0300

    files parsed

--- adsbparser.cpp
+++ adsbparser.cpp
@@ -2,6 +2,9 @@
 #include "ui_adsbparser.h"
 #include <QFile>
 #include <QFileDialog>
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QJsonObject>
 #include <QDebug>
 
 
@@ -28,6 +31,17 @@ AdsbParser::~AdsbParser()
 void AdsbParser::on_pbFileSelect_released()
 {
     QStringList fileNames = QFileDialog::getOpenFileNames(0, "input files", qApp->applicationDirPath(), "*.json", 0);
+    if (fileNames.isEmpty()) {
+        return;
+    }
+
+    ui->pbFileSelect->setDisabled(true);
+    ui->pbFileSelect->setText("working");
+
+    QFile outFile("adsb.txt");
+    outFile.open(QIODevice::WriteOnly);
+
+    QTextStream out(&outFile);
 
     for(QString fileName : fileNames)
     {
@@ -37,10 +51,52 @@ void AdsbParser::on_pbFileSelect_released()
             continue;
         }
 
-
-
+        QByteArray ba = adsbFile.readAll();
         adsbFile.close();
+
+        if (ba.isEmpty()) {
+            qDebug() << "error reading file" << fileName;
+            continue;
+        }
+
+        QJsonParseError *parserError {NULL};
+        QJsonDocument jDoc = QJsonDocument::fromJson(ba, parserError);
+        if (parserError) {
+            qDebug() << parserError << fileName;
+            continue;
+        }
+
+        QJsonObject jObj = jDoc.object();
+        QJsonValue  jVal = jObj.value("acList");
+        QJsonArray  jArr = jVal.toArray();
+
+
+        quint32 time = jObj.value("stm").toVariant().toLongLong() / 1000;
+        out << time;
+
+        double lat {0}, lon {0};
+        int icao {0};
+
+        foreach (QJsonValue jArrValue, jArr) {
+            jObj = jArrValue.toObject();
+            if (jObj.contains("Lat") && jObj.contains("Long"))
+            {
+                icao    = jObj.value("Icao").toString().toInt(0, 16);
+                lat     = jObj.value("Lat").toDouble();
+                lon     = jObj.value("Long").toDouble();
+
+                out << char(9) << icao << char(9) << lat << char(9) << lon;
+            }
+        }
+        out << char(10);
+        qDebug() << "loaded" << fileName;
     }
+
+    out.flush();
+    outFile.close();
+
+    ui->pbFileSelect->setDisabled(false);
+    ui->pbFileSelect->setText("Select files");
 }
 
 
